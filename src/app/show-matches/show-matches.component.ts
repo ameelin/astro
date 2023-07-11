fb90f931d17f381efba12ad82279c80e32a1a994
Neal Ravindran <nravindran@sherpa6.com>
2023-07-10 22:17:44 -0400

    Missed as part of the 3rd commint

---
+++ src/app/show-matches/show-matches.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-show-matches',
+  templateUrl: './show-matches.component.html',
+  styleUrls: ['./show-matches.component.css']
+})
+export class ShowMatchesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
